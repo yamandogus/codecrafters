6737618a0cb27a7e1d4126d233834e5db5570828
Doğuş Yaman <yamandogus251@gmail.com>
2025-09-03 19:02:30 +0300

    Delete src directory

--- src/app/pages/jobs/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function JobsPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-gray-800">İş İlanları</h1>
-    </div>
-  );
-} 
--- src/app/pages/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Register } from "@/components/auth/register";
-
-export default function RegisterPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen py-8">
-      <Register />
-    </div>
-  );
-}
